098181b91f4aa52b88855cc5f439d49fac414727
Patxi Sanchis <patxisldaw19@gmail.com>
2019-10-03 18:28:07 +0200

    Experimento con colores2

--- AdiosMundo.java
+++ AdiosMundo.java
@@ -2,7 +2,7 @@ public class AdiosMundo { // Clase principal
   public static void main (String[] args){
     String naranja = "\033[33m";
     String verde = "\033[32m";
-	String reset = "\033[0m";
+    String reset = "\033[0m";
       System.out.println(naranja+"¡Adios mundo!"); //Imprime en naranja
       System.out.println(verde+"Pero... volveré"+reset); //Imprime en verde
   }
